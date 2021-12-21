ebe102e739b455d01601397856161df844f97985
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-12-20 22:33:45 -0600

    Loaders de fuentes

--- portafolio/webpack.config.js
+++ portafolio/webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+
     },
     resolve: {
         extensions: ['.js']
@@ -34,7 +36,22 @@ module.exports = {
             {
                 test: /\.png/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    },
+                }
             }
+
         ]
     },
     plugins: [
